a7b23ece04b88c9aa18cd2f04cc47321a7acd31a
HarryHills3588 <hsh47@case.edu>
2025-06-18 19:50:20 -0400

    build: market producer implementation

--- services/market/price-generators/market_price_gen.py
+++ services/market/price-generators/market_price_gen.py
@@ -1,6 +1,7 @@
 from price_generator import StockGenerator
 from infrastructure.postgres.models.stock_model import Stock
 from infrastructure.postgres.utils.connection import Connection
+import threading
 
 from dotenv import load_dotenv
 import os 
@@ -12,15 +13,23 @@ def main():
     user = os.getenv("POSTGRES_USER")
     password = os.getenv("POSTGRES_PASSWORD")
     db = os.getenv("POSTGRES_DB")
+    bootstrap_server = os.getenv("BOOTSTRAP_SERVER")
 
     connection = Connection(f"postgresql://{user}:{password}@localhost:5432/{db}")
     session = connection.get_session()
+    logger.info("session created")
 
     ## query the DB
-    ## get results from query
-    ## initialize market through threads
+    result = session.query(Stock).all()
+    logger.debug(f"queried stocks, result {result}")
     
-    logger.info("session created")
+    for stock in result:
+        generator = StockGenerator(stock.ticker, bootstrap_servers=bootstrap_server)
+        thread = threading.Thread(target=generator.start, daemon=True)
+        thread.start()
+        logger.debug(f"Thread started for {stock.ticker}")
+
+    thread.join()
 
 if __name__ == "__main__":
     main()
